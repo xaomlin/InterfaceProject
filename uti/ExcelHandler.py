d5d611e80a0859154b7aa0543166002366d7c416
linxiaomo <838303806@qq.com>
2021-01-24 19:04:33 +0800

    调试Jenkins

--- uti/ExcelHandler.py
+++ uti/ExcelHandler.py
@@ -12,20 +12,15 @@ class ExcelHandler(object):
 
     def get_excel_data(self):
         # 获取到book对象
-        # print(conf.TEST_CASE_PATH)
         book = xlrd.open_workbook(conf.TEST_CASE_PATH)
-        # print(book)
         # 获取sheet对象
         sheet = book.sheet_by_index(0)
         rows, cols = sheet.nrows, sheet.ncols
         l = []
-        # print(sheet.row_values(0))
         title = sheet.row_values(0)
-        # print(title)
         # 获取其他行
         for i in range(1, rows):
             l.append(dict(zip(title, sheet.row_values(i))))
-        # print(l)
         return l
 
 
@@ -37,9 +32,8 @@ class ExcelHandler(object):
         request_data = []
         excel_data = self.get_excel_data()
         for i in range(len(excel_data)):
-            is_run = excel_data[i].get('case_run')
-            if is_run == 'yes':
-                # print(excel_data[i])
+            is_run = excel_data[i].get('case_run').upper()
+            if is_run == 'YES':
                 request_data.append(excel_data[i])
         # print(request_data)
         return request_data
